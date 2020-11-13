ae6998f09e8db77ec26c9357221e2af7837625f6
chas-e <sigmapi76388@gmail.com>
2020-11-13 13:20:24 -0500

    adding form for user input

---
+++ js/script.js
@@ -0,0 +1,7 @@
+alert('Javascript is loaded');
+
+const h1El = document.querySelector('h1'); // cached element reference
+
+h1El.innerText = "Please Work!!"; // set rendered text content of an element and it's descendants (children)
+
+const someVarINeed = console.log("Hello Taj!");
