91c0b0b993911978ef0f9da8cf1c7f60202fe000
chas-e <sigmapi76388@gmail.com>
2020-11-13 13:31:12 -0500

    adding submit element and styling

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ alert('Javascript is loaded');
 
 const h1El = document.querySelector('h1'); // cached element reference
 
-h1El.innerText = "Please Work!!"; // set rendered text content of an element and it's descendants (children)
+h1El.innerText = "Resistance Is Futile"; // set rendered text content of an element and it's descendants (children)
 
 const someVarINeed = console.log("Hello Taj!");
