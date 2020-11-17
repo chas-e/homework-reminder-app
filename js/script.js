24192d7344e200866ac6ffc36add26c4bc98a251
chas-e <sigmapi76388@gmail.com>
2020-11-17 13:22:20 -0500

    adding elements and styles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 alert('Javascript is loaded');
 
-const h1El = document.querySelector('h1'); // cached element reference
+// constants
 
-h1El.innerText = "Resistance Is Futile"; // set rendered text content of an element and it's descendants (children)
+// cached element refs
+const h1El = document.querySelector('h1');
 
-const someVarINeed = console.log("Hello Taj!");
+// App State
+const assignmentEls = [
+    {dueDateEl: 2020-11-16, 
+    classEl: 'English', 
+    assignmentDetailsEl: 'write a paper on some author',
+
+}, 'strings', 23, 23.4, true];
+
+console.log('assignmentEls', assignmentEls[0]);
+
+
+// event listeners
+
+
+
+// functions
+
+
+
+
+// render method
+
+
